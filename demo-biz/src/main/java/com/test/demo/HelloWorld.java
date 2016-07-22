e42c229fb7403e2892e83db743e566b9f806ec61
zhaohan <zhzhaohan@qq.com>
2016-07-22 12:14:18 +0800

    init

---
+++ demo-biz/src/main/java/com/test/demo/HelloWorld.java
@@ -0,0 +1,10 @@
+package com.test.demo;
+
+/**
+ * Created by Zi0000350 on 2016/7/22.
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
